6d6ffed9d2a619ab26cf6881582540c94e70a604
mFran <mariafrancisca@Mfrankika.local>
2022-04-13 15:48:04 +0100

    Base Estruturada

---
+++ README.md
@@ -0,0 +1,8 @@
+# Calculadora JavaScript
+
+[![MFrankika](https://www.linkedin.com/in/mfrankikaocruz)](https://www.creativebizdesign.com)
+
+Calculadora desenvolvida JavaScript.
+
+
+
---
+++ scripts/controller/CalcController.js
@@ -0,0 +1,52 @@
+class CalcController {
+  constructor() {
+    this._locale = 'pt-BR';
+    this._displayCalcEl = document.querySelector("#display");
+    this._dateEl = document.querySelector("#data");
+    this._timeEl = document.querySelector("#hora");
+    this._currentDate;
+    this.initialize();
+  }
+
+  initialize() {
+    this.setDisplayDataTime();
+    setInterval(() => {
+      this.setDisplayDataTime();
+    }, 1000); // = 1s
+  } 
+
+  setDisplayDataTime(){
+    this.displayDate = this.currentDate.toLocaleDateString(this._locale,{
+      day:"2-digit",
+      month:"long",
+      year:"numeric"
+    });
+    this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+  }
+  
+  get displayTime(){
+    return this._timeEl.innerHTML;
+  }
+
+  set displayTime(value){
+      return this._timeEl.innerHTML = value;
+  }
+  get displayDate(){
+      return this._dateEl.innerHTML;
+  }
+  set displayDate(value){
+      return this._dateEl.innerHTML = value;
+  }
+  get displayCalc(){
+      return this._displayCalcEl.innerHTML;
+  }
+  set displayCalc(value){
+      this._displayCalcEl.innerHTML = value;
+  }
+  get currentDate(){
+      return new Date();
+  }
+  set currentDate(value){
+      this._currentDate = value;
+  }
+}
